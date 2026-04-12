7b7ffce6e248fde551e24751ca09b0f1abaa7e2e
SiriYeung <119325339+SiriYeung@users.noreply.github.com>
2026-04-12 20:36:47 +0000

    try output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
 
 export default defineConfig({
-  integrations: [],
-  output: 'server',
-  adapter: netlify(),
+  output: 'static',
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'zh'],

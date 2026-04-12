3a429da7826eae99d25fb5ed6bc0567ca29c8453
Sam Li <weisen.li@hotmail.com>
2026-04-12 17:08:16 +0000

    hide faq menu

--- src/utils/menuItems.ts
+++ src/utils/menuItems.ts
@@ -10,7 +10,7 @@ export function getMenuItems(currentLang: string): MenuItem[] {
   return [
     { key: 'googleAdsAudit', href: getMenuLink('google-ads-audit', currentLang) },
     { key: 'googleAdsManagement', href: getMenuLink('google-ads-management', currentLang) },
-    { key: 'googleAdsFaq', href: getMenuLink('google-ads-faq', currentLang) },
+    // { key: 'googleAdsFaq', href: getMenuLink('google-ads-faq', currentLang) },
     { key: 'caseStudy', href: getMenuLink('case-study', currentLang) },
     { key: 'aboutUs', href: getMenuLink('about-us', currentLang) },
     { key: 'blogs', href: getMenuLink('blogs', currentLang) },
@@ -22,7 +22,7 @@ export function getHeaderMenuItems(currentLang: string): MenuItem[] {
   return [
     { key: 'googleAdsAudit', href: getMenuLink('google-ads-audit', currentLang) },
     { key: 'googleAdsManagement', href: getMenuLink('google-ads-management', currentLang) },
-    { key: 'googleAdsFaq', href: getMenuLink('google-ads-faq', currentLang) },
+    // { key: 'googleAdsFaq', href: getMenuLink('google-ads-faq', currentLang) },
     { key: 'caseStudy', href: getMenuLink('case-study', currentLang) },
     { key: 'aboutUs', href: getMenuLink('about-us', currentLang) },
     // { key: 'blogs', href: getMenuLink('blogs', currentLang) },

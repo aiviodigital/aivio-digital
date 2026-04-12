049affd3e6cf0ed51550dc0a52c33d2d5923d916
aiviodigital <info@aivio-digital.com>
2026-04-12 18:10:43 +0000

    add node server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
 
 export default defineConfig({
   integrations: [],
+  adapter: node({
+    mode: 'standalone'
+  }),
   output: 'hybrid',
   i18n: {
     defaultLocale: 'en',
